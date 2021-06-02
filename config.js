9cf8a37c421c891b283665335d54f78cd3beefb5
Mar Alegre <mar@mar.alemor.org>
2021-06-02 17:31:38 -0400

    split api key into config file, redid functionality so you can modify either amount

---
+++ config.js
@@ -0,0 +1,1 @@
+apiKey = 'c9555a7ee515159104fdba587ac2b480';
--- converter.js
+++ converter.js
@@ -1,41 +1,35 @@
-// basic variables
-let apiKey = 'c9555a7ee515159104fdba587ac2b480';
-let rates;
+// global variables
+let rates, apiKey;
 
 // functions
-async function fetchRates(apiKey) {
+async function fetchRates(apiKey,id) {
     let apiUrl = 'http://api.exchangeratesapi.io/v1/latest?access_key=' + apiKey;
 
     let response = await fetch(apiUrl);
     
     if (!response.ok) {
-	document.getElementById("fetchResult").innerHTML = "<b>Error:</b> fetching conversion rates failed!";
+	document.getElementById(id).innerHTML = "<b>Error:</b> fetching conversion rates failed!";
 	return
     }
     let result = await response.json();
     if (!result.success) {
-	document.getElementById("fetchResult").innerHTML = "<b>Error:</b> fetching conversion rates failed!";
+	document.getElementById(id).innerHTML = "<b>Error:</b> fetching conversion rates failed!";
 	return
     }
     rates = result.rates;
     let fetchDate = new Date(result.timestamp);
-    document.getElementById("fetchResult").innerHTML = "Conversion rates last updated on: " + fetchDate;
-    
-    let typeOptions = "";
-    for (let key in rates) {
-	typeOptions += '<option value="' + key + '">' + key + '</option>'
-    }
-
-    document.getElementById("typeIn").innerHTML = typeOptions;
-    document.getElementById("typeOut").innerHTML = typeOptions;
+    document.getElementById(id).innerHTML = "Conversion rates last updated on: " + fetchDate;
 }
 
-// convert function
 function convert(amt, curIn, curOut) {
     return amt / rates[curIn] * rates[curOut];
 }
 
-// fetch the rates
-fetchRates(apiKey);
+function listOptions(id) {
+    let options = "";
+    for (let key in rates) {
+	options += '<option value="' + key + '">' + key + '</option>'
+    }
 
-
+    document.getElementById(id).innerHTML = options;
+}
